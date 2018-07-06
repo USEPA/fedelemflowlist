8d81f2a2b7ba7a0b51451de488e8cc22f9dbabe2
WesIngwersen <ingwersen.wesley@epa.gov>
2018-07-06 16:09:47 -0400

    Add global variables file

---
+++ fedelemflowlist/globals.py
@@ -0,0 +1,10 @@
+#Set global variables for flow list creation
+
+inputpath = 'fedelemflowlist/input/'
+outputpath = 'fedelemflowlist/output/'
+
+context_fields = ['Directionality','Compartment']
+
+list_version_no = '0.1a' #a = alpha, b = beta
+
+flow_types = ['Energy','Fuel']
