4794d1eed1c4a5ba1d34fd055e6da7ac1bf5b784
WesIngwersen <ingwersen.wesley@epa.gov>
2018-07-06 16:25:42 -0400

    Update json-ld writer

--- fedelemflowlist/jsonld_writer.py
+++ fedelemflowlist/jsonld_writer.py
@@ -1,38 +1,38 @@
 #Adapted from https://github.com/USEPA/IO-Model-Builder/blob/master/iomb/olca/__init__.py
 
-#Not fully functional yet this is just a planned approach for writing the json-ld
-
 import json
 import logging as log
 import zipfile as zipf
 import pandas as pd
+import time
 
-#import list here from python pickle into pandas dataframe
-elemflowlist = pd.read_pickle('./output/ElementaryFlowList')
-#compartments = pd.read_pickle('./output/Compartments')
+from fedelemflowlist.globals import outputpath,list_version_no
 
-#merge with list
-
-zip_file = './output/testflowlistjson-ld.zip'
-
-pack = zipf.ZipFile(zip_file, mode='a', compression=zipf.ZIP_DEFLATED)
-
-#loop through rows, writing each elementary flow to the pack
-for index, row in elemflowlist.iterrows():
-    write_elemetary_flows(row,pack)
-pack.close()
-
+def write_flow_list_to_jsonld(elemflowlist):
+    zip_file = outputpath + 'Flows_' + list_version_no + '_json-ld.zip'
+    pack = zipf.ZipFile(zip_file, mode='a', compression=zipf.ZIP_DEFLATED)
+    #loop through rows, writing each elementary flow to the pack
+    for index, row in elemflowlist.iterrows():
+        write_elemetary_flows(row,pack)
+    pack.close()
 
 def write_elemetary_flows(flow: pd.Series, pack: zipf.ZipFile):
-    unit = flow['unit']
+    unit = flow['Unit']
     if unit is None:
         log.error('unknown unit %s in flow %s')
     f = {
         "@context": "http://greendelta.github.io/olca-schema/context.jsonld",
         "@type": "Flow",
-        "@id": flow['uuid'],
-        "name": flow['flowable'],
-        #"cas": flow['cas'],
+        "@id": flow['Flow UUID'],
+        "name": flow['Flowable'],
+        "cas": flow['CAS No'],
+        "version": list_version_no,
+        #Need to  determine time
+        #"lastChange": time.time()
+        #Category
+        #"category:":{
+        #"categoryPath":["Elementary flows",flow['Directionality'],flow['Compartment']]
+        #},
         "flowType": "ELEMENTARY_FLOW",
         "flowProperties": [{
             "@type": "FlowPropertyFactor",
@@ -40,16 +40,15 @@ def write_elemetary_flows(flow: pd.Series, pack: zipf.ZipFile):
             "conversionFactor": 1.0,
             "flowProperty": {
                 "@type": "FlowProperty",
-                "name": "",
-                "@id": ""}
+                "name": flow['Flow quality'],
+                "@id": flow["Quality UUID"]}
             }]}
-    compartment = flow['context']
+    #compartment = flow['Context UUID']
     #look up compartment id
     #compartmentid = compartments[compartments['name'] == compartment, uuid]
     #if compartment is not None:
     #    f["category"] = {"@type": "Category", "@id":compartmentid }
     dump(f, 'flows', pack)
-
 
 def dump(obj: dict, folder: str, pack: zipf.ZipFile):
     """ dump writes the given dictionary to the zip-file under the given folder.
