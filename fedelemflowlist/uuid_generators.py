3229d313ced9838c4eadf3be03df95ae2e3617c3
WesIngwersen <ingwersen.wesley@epa.gov>
2018-07-06 16:09:21 -0400

    Move uuid functions to this file and rename

---
+++ fedelemflowlist/uuid_generators.py
@@ -0,0 +1,16 @@
+#Generate UUIDs for flows
+#Uses UUID3 function from UUID package to generate IDs from strings
+
+import uuid
+
+def generate_flow_uuid(flowable,directionality,compartment,unit):
+    flow = flowable + directionality + compartment + unit
+    flowid = str(uuid.uuid3(uuid.NAMESPACE_OID,flow))
+    return flowid
+
+def generate_context_uuid(directionality,compartment):
+    context = directionality + compartment
+    contextid = str(uuid.uuid3(uuid.NAMESPACE_OID,context))
+    return contextid
+
+
