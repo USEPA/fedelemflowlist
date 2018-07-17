bd863814997148864c2f1f94d254f5bc008db1e1
WesIngwersen <ingwersen.wesley@epa.gov>
2018-07-16 23:08:14 -0400

    Major update
    Write categories, including compartment, directionality, and main Elementary flow
    Add category to flow writing

--- fedelemflowlist/jsonld_writer.py
+++ fedelemflowlist/jsonld_writer.py
@@ -1,19 +1,67 @@
-#Adapted from https://github.com/USEPA/IO-Model-Builder/blob/master/iomb/olca/__init__.py
+#Adapted from iomb.olca, currently at https://github.com/USEPA/IO-Model-Builder/blob/master/iomb/olca/__init__.py
+#For now this does not pack olca unit groups and flows properties, but just links to the relevant flow properties in the current olca data
 
 import json
 import logging as log
 import zipfile as zipf
 import pandas as pd
+import os
 import time
 
 from fedelemflowlist.globals import outputpath,list_version_no
 
-def write_flow_list_to_jsonld(elemflowlist):
+#Taken from an exported json-ld file. For the context.json file
+context_info = {"@vocab":"http://openlca.org/schema/v1.1/",
+                "@base":"http://openlca.org/schema/v1.1/",
+                "modelType":{"@type":"@vocab"},
+                "flowPropertyType":{"@type":"@vocab"},
+                "flowType":{"@type":"@vocab"},
+                "distributionType":{"@type":"@vocab"},
+                "parameterScope":{"@type":"@vocab"},
+                "allocationType":{"@type":"@vocab"},
+                "defaultAllocationMethod":{"@type":"@vocab"},
+                "allocationMethod":{"@type":"@vocab"},
+                "processType":{"@type":"@vocab"},
+                "riskLevel":{"@type":"@vocab"}}
+
+#Taken from an exported json-ld file. For the context.json file
+elementary_flows_category = {"@context":"http://greendelta.github.io/olca-schema/context.jsonld",
+                                 "@type":"Category",
+                                 "@id":"f318fa60-bae9-361f-ad5a-5066a0e2a9d1",
+                                 "name":"Elementary flows",
+                                 "modelType":"FLOW"}
+
+resource_flow_category = {"@context":"http://greendelta.github.io/olca-schema/context.jsonld",
+                          "@type":"Category",
+                          "@id":"3095c63c-7962-4086-a0d7-df4fd38c2e68",
+                          "name":"resource",
+                          "category":{"@type":"Category",
+                                      "@id":"f318fa60-bae9-361f-ad5a-5066a0e2a9d1",
+                                      "name":"Elementary flows"},
+                          "modelType":"FLOW"}
+emission_flow_category = {"@context":"http://greendelta.github.io/olca-schema/context.jsonld",
+                          "@type":"Category",
+                          "@id":"c2433915-9ca3-3933-a64d-68d67e3e3281",
+                          "name":"emission",
+                          "category": {"@type": "Category",
+                                       "@id": "f318fa60-bae9-361f-ad5a-5066a0e2a9d1",
+                                       "name": "Elementary flows"},
+                          "modelType":"FLOW"}
+
+def write_flow_list_to_jsonld(elemflowlist,contexts):
     zip_file = outputpath + 'FedElemFlowList_' + list_version_no + '_json-ld.zip'
+    #Need to remove existing file if it exists
+    if(os.path.exists(zip_file)):
+        os.remove(zip_file)
     pack = zipf.ZipFile(zip_file, mode='a', compression=zipf.ZIP_DEFLATED)
     #loop through rows, writing each elementary flow to the pack
     for index, row in elemflowlist.iterrows():
         write_elemetary_flows(row,pack)
+    for index, row in contexts.iterrows():
+        write_compartment_categories(row,pack)
+    write_directionality_categories(pack)
+    contexts_json = json.dumps(context_info)
+    pack.writestr('context.json', contexts_json)
     pack.close()
 
 def write_elemetary_flows(flow: pd.Series, pack: zipf.ZipFile):
@@ -30,9 +78,9 @@ def write_elemetary_flows(flow: pd.Series, pack: zipf.ZipFile):
         #Need to  determine time
         #"lastChange": time.time()
         #Category
-        #"category:":{
-        #"categoryPath":["Elementary flows",flow['Directionality'],flow['Compartment']]
-        #},
+        "category:": {"@type":"Category",
+                      "@id": flow["Compartment UUID"],
+                      "name": flow["Compartment"]},
         "flowType": "ELEMENTARY_FLOW",
         "flowProperties": [{
             "@type": "FlowPropertyFactor",
@@ -43,12 +91,31 @@ def write_elemetary_flows(flow: pd.Series, pack: zipf.ZipFile):
                 "name": flow['Flow quality'],
                 "@id": flow["Quality UUID"]}
             }]}
-    #compartment = flow['Context UUID']
-    #look up compartment id
-    #compartmentid = compartments[compartments['name'] == compartment, uuid]
-    #if compartment is not None:
-    #    f["category"] = {"@type": "Category", "@id":compartmentid }
     dump(f, 'flows', pack)
+
+def write_compartment_categories(category,pack):
+    #loop through contexts to create context for compartments
+    c = {
+        "@context": "http://greendelta.github.io/olca-schema/context.jsonld",
+        "@type": "Category",
+        "@id": category['Compartment UUID'],
+        "name": category["Compartment"],
+        "modelType": "FLOW"
+     }
+    if category["Directionality"]=="resource":
+        c["category"] =  {"@type": "Category",
+                          "@id": resource_flow_category["@id"],
+                           "name": resource_flow_category["name"]}
+    elif category["Directionality"]=="emission":
+        c["category"] = {"@type": "Category",
+                         "@id": emission_flow_category["@id"],
+                         "name": emission_flow_category["name"]}
+    dump(c,'categories',pack)
+
+def write_directionality_categories(pack):
+    dump(resource_flow_category,'categories',pack)
+    dump(emission_flow_category,'categories',pack)
+    dump(elementary_flows_category,'categories',pack)
 
 def dump(obj: dict, folder: str, pack: zipf.ZipFile):
     """ dump writes the given dictionary to the zip-file under the given folder.
@@ -60,3 +127,4 @@ def dump(obj: dict, folder: str, pack: zipf.ZipFile):
     path = '%s/%s.json' % (folder, obj['@id'])
     s = json.dumps(obj)
     pack.writestr(path, s)
+
