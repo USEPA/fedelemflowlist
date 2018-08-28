ae0347fc2d86abdb3db5235d41a0e09a2052819e
WesIngwersen <ingwersen.wesley@epa.gov>
2018-08-28 15:11:18 -0400

    Add flowmappingpath

--- fedelemflowlist/globals.py
+++ fedelemflowlist/globals.py
@@ -2,7 +2,7 @@
 
 inputpath = 'fedelemflowlist/input/'
 outputpath = 'fedelemflowlist/output/'
-
+flowmappingpath = 'fedelemflowlist/flowmapping'
 context_fields = ['Directionality','Compartment']
 
 list_version_no = '0.1' #Must be numeric
