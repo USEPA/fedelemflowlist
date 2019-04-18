55f28775ef191f954321cb659256e38a35eefe5c
WesIngwersen <ingwersen.wesley@epa.gov>
2019-04-17 22:40:22 -0400

    Move as_path and convert_to_lower to globals

--- fedelemflowlist/globals.py
+++ fedelemflowlist/globals.py
@@ -8,6 +8,20 @@ context_fields = ['Directionality','Compartment']
 #list_version_no = '0.1' #Must be numeric
 #flow_types = {'Energy':'resource', 'Fuel':'resource', 'Land':'resource', 'Chemicals':'emission', 'Groups':'emission'}
 
-list_version_no = '0.2' #Must be numeric
-flow_types = ['Biological','Chemicals','Energy', 'Fuel', 'Geological','Groups','Land','Water','Other']
+list_version_no = '0.3e' #Must be numeric
+#flow_types = ['Biological','Chemicals','Energy', 'Fuel', 'Geological','Groups','Land','Water','Other']
+flow_types = ['Energy']
+
+def convert_to_lower(x):
+    x = str(x)
+    x = str.lower(x)
+    return x
+
+def as_path(*args: str) -> str:
+    strings = []
+    for arg in args:
+        if arg is None:
+            continue
+        strings.append(str(arg).strip().lower())
+    return "/".join(strings)
 
--- fedelemflowlist/uuid_generators.py
+++ fedelemflowlist/uuid_generators.py
@@ -2,6 +2,7 @@
 #Uses UUID3 function from UUID package to generate IDs from strings
 
 import uuid
+from fedelemflowlist.globals import as_path
 
 def generate_flow_uuid(flowable,directionality,compartment,unit):
     flow = flowable + directionality + compartment + unit
@@ -17,10 +18,4 @@ def make_uuid(*args: str) -> str:
     path = as_path(*args)
     return str(uuid.uuid3(uuid.NAMESPACE_OID, path))
 
-def as_path(*args: str) -> str:
-    strings = []
-    for arg in args:
-        if arg is None:
-            continue
-        strings.append(str(arg).strip().lower())
-    return "/".join(strings)
+
