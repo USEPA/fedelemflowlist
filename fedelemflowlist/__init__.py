2713af3b6a80f93f76d73b20f011b8aaba1f0d85
WesIngwersen <ingwersen.wesley@epa.gov>
2019-05-22 10:14:31 -0400

    Major updates to API functions
    -get_flows replaces get_flowlist
    -remove list_flowlists and identify_most_recent_list
    -minor changes to get_flowmapping to work with new mapping format
    -add write_jsonld

--- fedelemflowlist/__init__.py
+++ fedelemflowlist/__init__.py
@@ -1,46 +1,27 @@
 import pandas as pd
-import os
 from fedelemflowlist.globals import outputpath, flowmappingpath
+import fedelemflowlist.jsonld as jsonld
 
 #Returns the most recent flows list as a dataframe by default
-def get_flowlist(version=None,format='df'):
-    if version is None:
-        list = identify_most_recent_list()
-    else:
-        list = 'FedElemFlowList_' + version
+def get_flows(preferred_only=None):
     #Get it as a dataframe
-    list_file = outputpath + list + '.csv'
-    if format == 'df':
-        flowlist = pd.read_csv(list_file,header=0)
-    return flowlist
-
-def list_flowlists():
-    files = os.listdir(outputpath)
-    flowslists = []
-    for name in files:
-        if name.endswith(".csv"):
-            n = name.strip('.csv')
-            flowslists.append(n)
-    return flowslists
-
-def identify_most_recent_list(list_type='any'):
-   flowlists =  list_flowlists()
-
-   #temp
-   return str(flowlists[0])
+    list_file = outputpath + 'FedElemFlowListMaster.csv'
+    flows = pd.read_csv(list_file,header=0)
+    if preferred_only:
+        flows = flows[flows['Preferred']==1]
+    return flows
 
 #Return a dataframe of the mapping file specific to the version of the list being used
 #If a soruce list is provided, it tries to filter the dataframe to return only the desired mappings
-def get_flowmapping(version=None,source_list=None):
-    if version is None:
-        list = identify_most_recent_list()
-    else:
-        list = 'FedElemFlowList_' + version
-    mapping_file = flowmappingpath + list + '_mapping.csv'
-
+def get_flowmapping(version=None,source=None):
+    mapping_file = flowmappingpath + 'FedElemFlowList_0.1' + '_mapping.csv'
     try: flowmapping =  pd.read_csv(mapping_file,header=0)
-    except FileNotFoundError: print("No mapping file found for list " + list)
-
-    if source_list is not None:
-        flowmapping = flowmapping[flowmapping['Source'].isin(source_list)]
+    except FileNotFoundError: print("No mapping file found for " + str(source))
+    if source is not None:
+        flowmapping = flowmapping[flowmapping['SourceListName'].isin(source)]
     return flowmapping
+
+def write_jsonld(flows,path):
+    writer = jsonld.Writer(flow_list=flows)
+    writer.write_to(path)
+
