40e552b55ced6526d4aebe0c619ac0d39210a778
WesIngwersen <ingwersen.wesley@epa.gov>
2019-06-08 16:42:47 -0400

    Clean up with PEP 8 and pylint

--- fedelemflowlist/__init__.py
+++ fedelemflowlist/__init__.py
@@ -1,7 +1,13 @@
+"""fedelemflowlist
+
+Allows retrieval of the Federal LCA Commons flow list and mapping files
+in standard pandas dataframe formats, defined within format specs
+"""
+import os
 import pandas as pd
 from fedelemflowlist.globals import outputpath, flowmappingpath
 import fedelemflowlist.jsonld as jsonld
-import os
+
 
 def get_flows(preferred_only=None):
     """Gets a flow list in a standard format
@@ -11,10 +17,11 @@ def get_flows(preferred_only=None):
     :return: standard Flow List dataframe
     """
     list_file = outputpath + 'FedElemFlowListMaster.csv'
-    flows = pd.read_csv(list_file,header=0)
+    flows = pd.read_csv(list_file, header=0)
     if preferred_only:
-        flows = flows[flows['Preferred']==1]
+        flows = flows[flows['Preferred'] == 1]
     return flows
+
 
 def get_flowmapping(source=None):
     """Gets a flow mapping in standard format
@@ -37,22 +44,23 @@ def get_flowmapping(source=None):
             except FileNotFoundError:
                 print("No mapping file found for " + str(f))
     else:
-        #load all mappings in directory
+        # load all mappings in directory
         files = os.listdir(flowmappingpath)
         for name in files:
             if name.endswith(".csv"):
                 flowmapping = pd.read_csv(flowmappingpath+name, header=0)
-                flowmappings = pd.concat([flowmappings,flowmapping])
+                flowmappings = pd.concat([flowmappings, flowmapping])
     return flowmappings
 
-def write_jsonld(flows,path,mappings=None):
-    """ Writes a standard openLCA JSON-LD zip archive with elementary flows and optionally flowmappings
+
+def write_jsonld(flows, path, mappings=None):
+    """ Writes a standard openLCA JSON-LD zip archive with elementary flows and optionally
+     flowmappings
 
     :param flows: standard pd Flow List dataframe, generally from get_flows()
     :param path: path and filename with .zip extention, e.g. 'c:users/mai/fedcommonsflows.zip'
     :param mappings: standard pd Flow Mapping dataframe , generally from get_flowmapping()
     :return: writes out .zip file
     """
-    writer = jsonld.Writer(flow_list=flows,flow_mapping=mappings)
+    writer = jsonld.Writer(flow_list=flows, flow_mapping=mappings)
     writer.write_to(path)
-
--- fedelemflowlist/globals.py
+++ fedelemflowlist/globals.py
@@ -1,36 +1,48 @@
-#Set global variables for flow list creation
+# Set global variables for flow list creation
 import sys
 import os
 import json
+import logging as log
 
-try: modulepath = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') + '/'
-except NameError: modulepath = 'fedelemflowlist/'
+try:
+    modulepath = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/') + '/'
+except NameError:
+    modulepath = 'fedelemflowlist/'
 
 outputpath = modulepath + 'output/'
 inputpath = modulepath + 'input/'
 flowmappingpath = modulepath + 'flowmapping/'
 
-import logging as log
 log.basicConfig(level=log.DEBUG, format='%(levelname)s %(message)s',
                 stream=sys.stdout)
 
 try:
-    with open(modulepath +"flowlistspecs.json") as cfg:
+    with open(modulepath + "flowlistspecs.json") as cfg:
         flow_list_specs = json.load(cfg)
 except FileNotFoundError:
     log.info("Flow list specs not found. Create a flow list specs file.")
 
 
 def convert_to_lower(x):
+    """Convert string to lower case
+
+    :param x: string
+    :return: string
+    """
     x = str(x)
     x = str.lower(x)
     return x
 
+
 def as_path(*args: str) -> str:
+    """Converts strings to lowercase path-like string
+    Take variable order of string inputs
+    :param args: variable-length of strings
+    :return: string
+    """
     strings = []
     for arg in args:
         if arg is None:
             continue
         strings.append(str(arg).strip().lower())
     return "/".join(strings)
-
