7f5475f82fd1363f8bafa4d36d0ed1572de3dec4
WesIngwersen <ingwersen.wesley@epa.gov>
2019-07-11 16:40:43 -0400

    -Linting -Add test to see there are no nas in required fields

--- tests/test_flow_list.py
+++ tests/test_flow_list.py
@@ -13,10 +13,15 @@ fulllist_rand10 = fulllist.sample(n=10)
 fulllist_rand10_selectcols = fulllist_rand10[['Flowable','Context','Unit','Flow UUID']].reset_index()[['Flowable','Context','Unit','Flow UUID']]
 fulllist_rand10_selectcols.to_dict()
 """
-existing_flow_list_sample = {'Flowable': {0: 'Copper octanoate', 1: "3,3',4,4'-Tetrachlorodiphenyl ether",
-                                          2: 'Ethyl oxalate', 3: 'Disodium methanearsonate',
-                                          4: '3,3-Dimethyl-2-butanol', 5: 'Bis(trichloromethyl) sulfone',
-                                          6: 'Ethopabate', 7: 'Etrimfos', 8: '2,3,6-Trichlorobenzoic acid',
+existing_flow_list_sample = {'Flowable': {0: 'Copper octanoate',
+                                          1: "3,3',4,4'-Tetrachlorodiphenyl ether",
+                                          2: 'Ethyl oxalate',
+                                          3: 'Disodium methanearsonate',
+                                          4: '3,3-Dimethyl-2-butanol',
+                                          5: 'Bis(trichloromethyl) sulfone',
+                                          6: 'Ethopabate',
+                                          7: 'Etrimfos',
+                                          8: '2,3,6-Trichlorobenzoic acid',
                                           9: 'Nithiazide'},
                              'Context': {0: 'emission/ground/terrestrial/wetland',
                                          1: 'emission/water/fresh water body/lake/rural',
@@ -42,22 +47,41 @@ existing_flow_list_sample = {'Flowable': {0: 'Copper octanoate', 1: "3,3',4,4'-T
                                            9: 'f08ce166-60e5-3d27-937d-d351b62d8569'}}
 existing_flow_sample = pd.DataFrame(existing_flow_list_sample)
 
+def get_required_flowlist_fields():
+    """Gets required field names for Flow List
+    :return:list of required fields
+    """
+    from fedelemflowlist.globals import flow_list_fields
+    required_fields = []
+    for k, v in flow_list_fields.items():
+        if v[1]['required']:
+            required_fields.append(k)
+    return required_fields
+
 class TestFlowList(unittest.TestCase):
 
     def setUp(self):
-        """
-        Get flowlist used for all tests
-        :return:
+        """Get flowlist used for all tests
         """
         self.flowlist = fedelemflowlist.get_flows()
 
+
     def test_UUID_stability(self):
+        """Checks that UUIDs in currently served list are identical to old UUIDs
         """
-        Checks that UUIDs in currently served list are identical to old UUIDs
-        :return:
+        merge_new_and_existing = pd.merge(existing_flow_sample, self.flowlist,
+                                          on=['Flowable', 'Context', 'Unit'])
+        self.assertEqual(set(merge_new_and_existing['Flow UUID_x']),
+                         set(merge_new_and_existing['Flow UUID_y']))
+
+
+    def test_no_nas_in_required_fields(self):
+        """Checks that no flows have na values in required fields
         """
-        merge_new_and_existing = pd.merge(existing_flow_sample,self.flowlist,on=['Flowable','Context','Unit'])
-        self.assertEqual(set(merge_new_and_existing['Flow UUID_x']),set(merge_new_and_existing['Flow UUID_y']))
+        required_fields = get_required_flowlist_fields()
+        flowlist_w_required = self.flowlist[required_fields]
+        nas_in_required = flowlist_w_required.dropna()
+        self.assertEqual(len(flowlist_w_required), len(nas_in_required))
 
 
 if __name__ == '__main__':
