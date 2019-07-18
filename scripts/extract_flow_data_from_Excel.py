327f751ca85b84d4df6f2757963143adb8e36cd2
WesIngwersen <ingwersen.wesley@epa.gov>
2019-07-18 17:16:02 -0400

    Add doc string and alt units to script

--- scripts/extract_flow_data_from_Excel.py
+++ scripts/extract_flow_data_from_Excel.py
@@ -1,15 +1,20 @@
-# Flow data were originally developed in Excel. This script extracts those data and writes them to csv files.
-# The flow class files should match the class names in the flowlistspecs
+"""
+Flow data were originally developed in Excel. This script extracts those data and writes them to csv files.
+WARNING: This will replace the existing input files
+The flow class files should match the class names in the flowlistspecs
+"""
+
 import pandas as pd
 from fedelemflowlist.globals import inputpath, flow_list_specs
 
-# Set directory where excel files..can put in same folder
+# Set directory where excel files..can put in same folder as csv input files
 
 excel_dir = inputpath
 
 if __name__ == '__main__':
     # Read in and write out needed sheets from flow class files
     for t in flow_list_specs["flow_classes"]:
+
         # Handle flowables first
         flowables_for_class = pd.read_excel(excel_dir + t + '.xlsx', sheet_name='Flowables', header=0)
         # Drop if the line is blank
@@ -21,6 +26,13 @@ if __name__ == '__main__':
         class_primary_contexts = class_primary_contexts.dropna(axis=0, how='all')
 
         class_primary_contexts.to_csv(inputpath + t + 'FlowablePrimaryContexts.csv', index=False)
+        #Try to extract alt unit files
+        try:
+            altlunits_for_class = pd.read_excel(excel_dir + t + '.xlsx', sheet_name='FlowableAltUnits', header=0)
+            altlunits_for_class = altlunits_for_class.dropna(axis=0, how='all')
+            altlunits_for_class.to_csv(inputpath + t + 'FlowableAltUnits.csv', index=False)
+        except:
+            continue
 
     contexts = pd.read_excel(excel_dir + 'Contexts.xlsx', sheet_name='Contexts', na_values='N/A')  #
     contexts.to_csv(inputpath + 'Contexts.csv', index=False)
