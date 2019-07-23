b525a38cb09bc138e9d66d1733aef959b20e46fd
WesIngwersen <ingwersen.wesley@epa.gov>
2019-07-23 11:19:11 -0400

    Write flow list summary to outputpath

--- scripts/summarize_flowlist.py
+++ scripts/summarize_flowlist.py
@@ -1,9 +1,10 @@
 import fedelemflowlist
 from fedelemflowlist.analysis.flow_list_analysis import *
+from fedelemflowlist.globals import outputpath
 
 all_flows = fedelemflowlist.get_flows()
 #preferred_flows = all_flows[all_flows['Preferred']==True]
 
 all_flows_counts = count_flows_by_class(all_flows)
 
-all_flows_counts.to_csv('work/all_flows_counts.csv',index=False)
+all_flows_counts.to_csv(outputpath+'all_flows_counts.csv',index=False)
