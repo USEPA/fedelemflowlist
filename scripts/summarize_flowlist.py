56c541f218c6acf7cead0d727773d4ae6b01f58d
WesIngwersen <ingwersen.wesley@epa.gov>
2019-07-24 19:09:55 -0400

    Add flowable counts and contexts

--- scripts/summarize_flowlist.py
+++ scripts/summarize_flowlist.py
@@ -2,9 +2,17 @@ import fedelemflowlist
 from fedelemflowlist.analysis.flow_list_analysis import *
 from fedelemflowlist.globals import outputpath
 
-all_flows = fedelemflowlist.get_flows()
-#preferred_flows = all_flows[all_flows['Preferred']==True]
+flowlist = fedelemflowlist.get_flows()
+preferred_flows = flowlist[flowlist['Preferred']==True]
 
-all_flows_counts = count_flows_by_class(all_flows)
-
+all_flows_counts = count_flows_by_class(flowlist)
 all_flows_counts.to_csv(outputpath+'all_flows_counts.csv',index=False)
+
+flowable_counts = count_flowables_by_class(flowlist)
+flowable_counts.to_csv(outputpath+'flowable_counts.csv',index=False)
+
+contexts = list_contexts(flowlist)
+contexts.to_csv(outputpath+'all_contexts.csv',index=False)
+
+preferred_contexts = list_contexts(preferred_flows)
+preferred_contexts.to_csv(outputpath+'preferred_contexts.csv',index=False)
