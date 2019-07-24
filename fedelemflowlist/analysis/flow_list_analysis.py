fba21e7c1bb60e5d7b3a2e724ade18487fca2bc6
WesIngwersen <ingwersen.wesley@epa.gov>
2019-07-24 19:09:34 -0400

    -Add count flowables by class -Add list_contexts -Revise count contexts to provide %

--- fedelemflowlist/analysis/flow_list_analysis.py
+++ fedelemflowlist/analysis/flow_list_analysis.py
@@ -10,8 +10,67 @@ def count_flows_by_class(flowlist):
     fl_pref = flowlist.groupby(['Class'])['Preferred'].sum().reset_index()
     fl_combined = pd.merge(fl_all,fl_pref,on=['Class'])
     fl_combined = fl_combined.rename(columns={'Flow UUID':'No. of Flows','Preferred':'No. of Preferred Flows'})
-    total_row = {'Class':'TOTAL','No. of Flows':fl_combined['No. of Flows'].sum(),'No. of Preferred Flows':fl_combined['No. of Preferred Flows'].sum()}
+    total_flows = fl_combined['No. of Flows'].sum()
+    fl_combined['Percent of Flows'] = fl_combined['No. of Flows'].apply(lambda x: str((x / total_flows)*100) + '%')
+    total_row = {'Class':'TOTAL','No. of Flows':total_flows,'No. of Preferred Flows':fl_combined['No. of Preferred Flows'].sum()}
     fl_combined = fl_combined.append(total_row,ignore_index=True)
     return fl_combined
+
+def list_contexts(flowlist):
+    """Displays list of contexts along with 1 if the context is present for that flowclass.
+    Can send a preferred list or
+        :param flowlist: A FlowList in standard format
+    :return: pandas df with 'Context',plus columns for all flow classes
+    """
+    contexts = flowlist[['Context','Class']]
+    contexts = contexts.drop_duplicates()
+    contexts['Num'] = 1
+    context_counts = contexts.pivot_table(index='Context',columns='Class',values='Num',aggfunc='count',fill_value='0').reset_index()
+    return context_counts
+
+def count_flowables_by_class(flowlist):
+    """
+    Counts flowables by class
+    :param flowlist: A FlowList in standard format
+    :return: pandas df with 'Class' and counts of unique flowables
+    """
+    flowables_by_class = flowlist[['Class','Flowable']]
+    flowables_by_class = flowables_by_class.drop_duplicates()
+    flowables_by_class_count = flowables_by_class.groupby('Class')['Flowable'].count().reset_index()
+    total = flowables_by_class_count['Flowable'].sum()
+    flowables_by_class_count['Percent'] = flowables_by_class_count['Flowable'].apply(lambda x: str((x / total)*100) + '%')
+    total_row = {'Class':'TOTAL','Flowable':total}
+    flowables_by_class_count = flowables_by_class_count.append(total_row,ignore_index=True)
+    return flowables_by_class_count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
