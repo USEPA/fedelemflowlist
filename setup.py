d8e07e61841d2396a4ff590b850e3f2e57f88075
WesIngwersen <ingwersen.wesley@epa.gov>
2019-09-13 10:57:34 -0400

    Update setup development status to production/stable

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Wesley Ingwersen',
     author_email='ingwersen.wesley@epa.gov',
     classifiers=[
-        "Development Status :: 4 - BETA",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: IDE",
         "Intended Audience :: Science/Research",
         "License :: CC0",
