b0b6594cf4038f0ffe8dcd142cc9f72fa55e55ea
WesIngwersen <ingwersen.wesley@epa.gov>
2020-01-08 16:02:32 -0500

    Update to compare previous release with current release

--- scripts/compare_old_flowlist.py
+++ scripts/compare_old_flowlist.py
@@ -6,12 +6,13 @@ Currently compares only by UUID.
 
 import pandas as pd
 import fedelemflowlist
-from fedelemflowlist.globals import outputpath
+from fedelemflowlist.globals import outputpath,flow_list_specs
 
 # get the current list
 
 # Enter name of old version here. Must be in output folder
-old_version_parquet = 'FedElemFlowListMaster_1.0rc4.parquet'
+old_version_parquet = 'FedElemFlowListMaster1.0.parquet'
+old_version = '1.0'
 
 if __name__ == '__main__':
     current_list = fedelemflowlist.get_flows()
@@ -27,9 +28,9 @@ if __name__ == '__main__':
 
     new_UUIDS = list(set(current_UUIDs) - set(old_list_UUIDs))
     new_flows = current_list[current_list['Flow UUID'].isin(new_UUIDS)]
-    new_flows.to_csv(outputpath + 'new_flows.csv', index=False)
-    fedelemflowlist.write_jsonld(new_flows, outputpath + 'FedElemFlowList_newflows.zip')
+    new_flows.to_csv(outputpath + 'new_flows' + old_version + 'to' + flow_list_specs['list_version'] +'.csv', index=False)
+    fedelemflowlist.write_jsonld(new_flows, outputpath + 'FedElemFlowList_newflows' + old_version + 'to' + flow_list_specs['list_version'] +'.zip')
     expired_UUIDs = list(set(old_list_UUIDs) - set(current_UUIDs))
     expired_flows = old_list[old_list['Flow UUID'].isin(expired_UUIDs)]
 
-    expired_flows.to_csv(outputpath + 'expired_flows.csv', index=False)
+    expired_flows.to_csv(outputpath + 'expired_flows' + old_version + 'to' + flow_list_specs['list_version'] +'.csv', index=False)
