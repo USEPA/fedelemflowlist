5f352a3ddc81b219c7bb02adfe6b3bc3b0888891
Ben Young <44471635+bl-young@users.noreply.github.com>
2020-07-18 14:40:09 -0400

    Update for freshwater_resources and added land_use

--- fedelemflowlist/subset_list.py
+++ fedelemflowlist/subset_list.py
@@ -6,15 +6,30 @@ Functions to filter a flow list for subsets
 Functions correspond with a subset names stored in the subsets dictionary
 """
 
-subsets = {"freshwater_resources":"get_freshwater_resource_flows"}
+subsets = {"freshwater_resources":"get_freshwater_resource_flows",
+           "land_use":"get_land_use_flows"}
 
 
 def get_freshwater_resource_flows(fl):
     """
+    Subsets the flow list for all freshwater resource flows
 
     :param fl: df in standard flowlist format
     :return: df in standard flowlist format
     """
     flows = fl[fl["Flowable"]=="Water, fresh"]
-    #! needs to be subset for resource flows
+    flows = flows[flows["Context"].str.startswith("resource")]
+    flows = flows[~flows["Context"].str.startswith("resource/air")]
+
+    return flows
+
+def get_land_use_flows(fl):
+    """
+    Subsets the flow list for all land use resource flows
+
+    :param fl: df in standard flowlist format
+    :return: df in standard flowlist format
+    """
+    flows = fl[fl["Class"]=="Land"]
+    
     return flows
