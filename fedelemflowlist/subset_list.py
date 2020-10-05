f512a2a3f09cca04d7390600c5f367266a2f0df7
Ben Young <44471635+bl-young@users.noreply.github.com>
2020-10-05 14:36:56 -0400

    update subset list for mineral resources, energy, metal emissions, HAPs

--- fedelemflowlist/subset_list.py
+++ fedelemflowlist/subset_list.py
@@ -6,9 +6,16 @@ Functions to filter a flow list for subsets
 Functions correspond with a subset names stored in the subsets dictionary
 """
 
+import pandas as pd
+from fedelemflowlist.globals import inputpath
+
 subsets = {"freshwater_resources":"get_freshwater_resource_flows",
            "water_resources":"get_water_resource_flows",
-           "land_use":"get_land_use_flows"}
+           "land_use":"get_land_use_flows",
+           "mineral_resources":"get_mineral_resource_flows",
+           "energy":"get_energy_flows",
+           "metal_emissions":"get_metal_emissions_flows",
+           "HAP":"get_hazardous_air_pollutant_flows"}
 
 
 def get_subsets() -> list():
@@ -59,3 +66,57 @@ def get_land_use_flows(fl):
     flows = fl[fl["Class"]=="Land"]
     
     return flows
+
+def get_mineral_resource_flows(fl):
+    """
+    Subsets the flow list for all mineral resource flows
+
+    :param fl: df in standard flowlist format
+    :return: df in standard flowlist format
+    """
+    flows = fl[fl["Class"]=="Geological"]
+    flows = flows[flows["Context"].str.startswith("resource")]
+    flows = flows[~flows["Flowable"].str.contains(" ore")]
+    flows = flows[flows["Unit"]=="kg"]
+    
+    return flows
+
+def get_energy_flows(fl):
+    """
+    Subsets the flow list for all energy flows
+
+    :param fl: df in standard flowlist format
+    :return: df in standard flowlist format
+    """
+    flows = fl[fl["Unit"]=="MJ"]
+    flows = flows[flows["Context"].str.startswith("resource")]
+        
+    return flows
+
+def get_metal_emission_flows(fl):
+    """
+    Subsets the flow list for all emissions of metals
+
+    :param fl: df in standard flowlist format
+    :return: df in standard flowlist format
+    """
+    flows = fl[fl["Context"].str.startswith("emission")]
+    #TODO Update with list of metals
+        
+    return flows
+
+def get_hazardous_air_pollutant_flows(fl):
+    """
+    Subsets the flow list for all HAP emissions
+
+    :param fl: df in standard flowlist format
+    :return: df in standard flowlist format
+    """
+    flows = fl[fl["Context"].str.startswith("emission/air")]
+    haps = pd.read_csv(inputpath+'HAP_flows.csv', usecols=['Flowable'])
+    # HAPs sourced from EPA via script write_HAP_flows.py
+    # https://www.epa.gov/haps/initial-list-hazardous-air-pollutants-modifications
+    flows = flows[flows['Flowable'].isin(haps.Flowable)]
+            
+    return flows
+    
