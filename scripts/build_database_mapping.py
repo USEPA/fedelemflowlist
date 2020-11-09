977bf0e055681df4ea0061d4eef672409aaffb7d
hottleta <troy.hottle@erg.com>
2020-11-09 10:38:37 -0500

    removed deletion of duplicates in order to retain split conversions, maintains warning to check for duplicate flows in source flowables list

--- scripts/build_database_mapping.py
+++ scripts/build_database_mapping.py
@@ -4,7 +4,7 @@ from fedelemflowlist.globals import inputpath, flowmapping_fields, log, flowmapp
     add_uuid_to_mapping, add_conversion_to_mapping
 
 #Input source name here. The .csv mapping file with this name must be in the input directory
-source_name = 'TRI'
+source_name = 'DMR'
 
 if __name__ == '__main__':
     # Pull flowable mapping file. Assume these are in input folder with source_name+FlowableMappings.csv
@@ -14,7 +14,6 @@ if __name__ == '__main__':
         log.warning('Duplicate source flows in flowables list:')
         dup = flowables.loc[flowables.duplicated(subset=['SourceFlowName'])]
         log.warning(dup.SourceFlowName.tolist())
-        flowables.drop_duplicates(subset=['SourceFlowName'],inplace=True, ignore_index=True)
     
     # Pull context mapping file. Assume these are in input folder with source_name+ContextMappings.csv
     context_mappings = pd.read_csv(inputpath + source_name + '_ContextMappings.csv')
