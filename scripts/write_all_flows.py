eb4a180fa774e495999de2edf55afb8526c71ea5
Ben Young <44471635+bl-young@users.noreply.github.com>
2021-03-18 10:07:30 -0400

    remove hyperlinks when writing to excel to avoid warnings and loss of URL in output file

--- scripts/write_all_flows.py
+++ scripts/write_all_flows.py
@@ -1,4 +1,6 @@
 """Writes preferred flows to a JSON-LD archive in the output folder."""
+import pandas as pd
+
 import fedelemflowlist
 from fedelemflowlist.globals import outputpath,flow_list_specs
 
@@ -7,4 +9,6 @@ if __name__ == '__main__':
 
     all_flows = fedelemflowlist.get_flows(preferred_only=False)
     fedelemflowlist.write_jsonld(all_flows,outputpath+'FedElemFlowList_'+flow_list_specs['list_version']+'_all.zip')
-    all_flows.to_excel(outputpath+'FedElemFlowList_'+flow_list_specs['list_version']+'_all.xlsx',index=False)
+    with pd.ExcelWriter(outputpath+'FedElemFlowList_'+flow_list_specs['list_version']+'_all.xlsx',
+                        options={'strings_to_urls':False}) as writer:
+        all_flows.to_excel(writer, index=False)
