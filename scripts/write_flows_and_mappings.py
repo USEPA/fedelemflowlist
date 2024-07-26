a1b87c46aaa7114b08cca98d675cbedc5677fe8d
Ben Young <ben.young@erg.com>
2024-07-26 12:26:31 -0400

    print output path

--- scripts/write_flows_and_mappings.py
+++ scripts/write_flows_and_mappings.py
@@ -19,10 +19,12 @@ if __name__ == '__main__':
 
     # Now write out flows and mappings
     export_name = ''
+    if isinstance(mapping_to_use, str):
+        mapping_to_use = [mapping_to_use]
     for s in mapping_to_use:
         export_name = export_name + s + '_'
     export_name = export_name + 'flows_w_mappings.zip'
     fedelemflowlist.write_jsonld(flows_used_in_mapping,
                                  outputpath / export_name,
                                  mapping)
-
+    print(f"File saved to {outputpath / export_name}")
